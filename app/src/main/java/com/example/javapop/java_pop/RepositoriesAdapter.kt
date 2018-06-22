e76e0b21ccbcb5126e375f25df1b5f46393c1b4d
Gabriel Esteves <gabriel.esteves@kanamobi.com.br>
2018-06-22 18:38:42 -0300

    Commit
    commit

--- app/src/main/java/com/example/javapop/java_pop/RepositoriesAdapter.kt
+++ app/src/main/java/com/example/javapop/java_pop/RepositoriesAdapter.kt
@@ -7,14 +7,24 @@ import android.view.View
 import android.view.ViewGroup
 import kotlinx.android.synthetic.main.list_viewholder.view.*
 
-class RepositoriesAdapter(val items: ArrayList<String>, val context: Context) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+class RepositoriesAdapter(items: ArrayList<String>,
+                          context: Context) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    private val items = items
+    private val context = context
+
+    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): RecyclerView.ViewHolder {
+        val view = LayoutInflater.from(context).inflate(R.layout.list_viewholder, parent, false)
+        return ViewHolder(view)
+        TODO("not impleme¥nted") //To change body of created functions use File | Settings | File Templates.
+    }
+
 
     override fun getItemCount(): Int {
         return items.size
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): RecyclerView.ViewHolder {
-        return ViewHolder(LayoutInflater.from(context).inflate(R.layout.list_viewholder, parent, false))
+    override fun onCreateViewHolder(view: View, position: Int): RecyclerView.ViewHolder {
+
     }
 
     override fun onBindViewHolder(holder: ViewHolder?, position: Int) {
@@ -26,13 +36,13 @@ class RepositoriesAdapter(val items: ArrayList<String>, val context: Context) : 
         holder?.numberFavorite?.text = items.get(position)
     }
 
-    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        val nameRepositories = view.tv_nameRepositories
-        val description = view.tv_description
-        val userName = view.tv_username
-        val lastName = view.tv_lastName
-        val numberFork = view.tv_numberFork
-        val numberFavorite = view.tv_numberFavorite
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val nameRepositories = itemView.tv_nameRepositories
+        val description = itemView.tv_description
+        val userName = itemView.tv_username
+        val lastName = itemView.tv_lastName
+        val numberFork = itemView.tv_numberFork
+        val numberFavorite = itemView.tv_numberFavorite
 
     }
 }
